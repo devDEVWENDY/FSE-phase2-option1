9baa6ad5e8d6bf463f623ad38d571f12fb9c8e07
Wendy <wen.mcnair@gmail.com>
2024-03-07 14:30:50 -0500

    added dummy text to test component

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -4,7 +4,7 @@ import React from 'react';
 function CustomerList() {
     return(
         <div className='customers'>
-            
+            <h1>THIS IS CUSTOMER LIST</h1>
         </div>
     );
 }
