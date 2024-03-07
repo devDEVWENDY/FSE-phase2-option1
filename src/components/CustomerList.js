8c150b6abd8a953d16e90340bb7ee7e2b207161b
Wendy <wen.mcnair@gmail.com>
2024-03-07 15:20:06 -0500

    transfer customers.map from App to CustomerList component

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 
-function CustomerList() {
+function CustomerList(props) {
+    console.log(`CustomerList props ${props.customers}`);
     return(
         <div className='customers'>
             <h1>THIS IS CUSTOMER LIST</h1>
+
+            {customers.map(
+                (item, index) => {
+                {/* className: checks customer id matches form id to set className to selected for this customer item */}
+
+                return (<tr key={item.id} 
+                    className={ (item.id === formObject.id )?'selected': ''}
+                    onClick={()=>handleListClick(item)} 
+                >
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.password}</td>
+                </tr>);
+                }
+            )}
+          
+            
         </div>
     );
 }
