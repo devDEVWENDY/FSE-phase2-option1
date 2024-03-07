bc9c66a2c331fd75e1293ea67f49a6f90687fe3c
Wendy <wen.mcnair@gmail.com>
2024-03-07 16:55:43 -0500

    cleaned up alignment for easier read

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -2,50 +2,45 @@ import React from 'react';
 
 
 function CustomerList(props) {
-console.log(`PROPS ${props}`)
+console.log(`PROPS ${props.customers[0]}`)
 
     return(
         <div className='customers'>
             <h1>THIS IS CUSTOMER LIST</h1>
 
             <div className="boxed" >
-        <h4>Customer List</h4>
-        <table id="customer-list">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Pass</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* below: maps over customers to create each row for a single customer item name, email, password */}
+                <h4>Customer List</h4>
+                <table id="customer-list">
+                <thead>
+                    <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Pass</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* below: maps over customers to create each row for a single customer item name, email, password */}
 
-            {props.customers.map(
-              (item, index) => {
-                {/* className: checks customer id matches form id to set className to selected for this customer item */}
+                    {props.customers.map(
+                    (item, index) => {
+                        {/* className: checks customer id matches form id to set className to selected for this customer item */}
 
-                return (<tr key={item.id} 
-                  className={ (item.id === props.formObject.id )?'selected': ''}
-                  onClick={()=>props.handleListClick(item)} 
-                >
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.password}</td>
-                </tr>);
-              }
-            )}
-          </tbody>
-        </table>
-    </div>
-
-
-
-
-
-
+                        return (<tr key={item.id} 
+                        className={ (item.id === props.formObject.id )?'selected': ''}
+                        onClick={()=>props.handleListClick(item)} 
+                        >
+                        <td>{item.name}</td>
+                        <td>{item.email}</td>
+                        <td>{item.password}</td>
+                        </tr>);
+                    }
+                    )}
+                </tbody>
+                </table>
+            </div>
         </div>
     );
+    
 }
 
 export default CustomerList;
